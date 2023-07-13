a4471c3feba8156a88209404114c360c27c92675
adilson-psantos <dipersan@gmail.com>
2023-07-13 01:12:09 -0300

    Realizada manipulação do Dom no titulo e texto da pagina tratamentos e aplicado dark-mode

--- JS/tratamentos.js
+++ JS/tratamentos.js
@@ -1,12 +1,8 @@
-let texto = document.getElementById(".titulo")
-titulo.innerText = "Energia Fria Vibracional Curadora - EFVC"
+let texto = document.getElementById("titulo");
+titulo.innerText = "Energia Fria Vibracional";
+console.log(titulo);
 
-const troca = document.querySelector(".texto-simples")
-troca.innerHTML = "Os Pleidianos são Seres de Luz estrelares que fazem parte da Constelação de Touro e atuam diretamente na consciência do Planeta Terra. Ligados também a Órion começaram a interferir para a manutenção do equilíbrio gravitacional e contribui para a elevação espiritual pela expansão da consciência que aos poucos se perde e desequilibra a Órbita do Eixo da Terra, afeta a humanidade. Como médicos energéticos do terceiro milênio, interferem naqueles com o DNA desperto para evolução que passam por vibração da Frequência de Luz!"
-console.log(troca);
-
-
-
-
-
-
+let descricao = document.querySelector(".texto-simples");
+descricao.innerHTML =
+  "Os Pleidianos são Seres de Luz estrelares que fazem parte da Constelação de Touro e atuam diretamente na consciência do Planeta Terra. Ligados também a Órion começaram a interferir para a manutenção do equilíbrio gravitacional e contribui para a elevação espiritual pela expansão da consciência que aos poucos se perde e desequilibra a Órbita do Eixo da Terra, afeta a humanidade. Como médicos energéticos do terceiro milênio, interferem naqueles com o DNA desperto para evolução que passam por vibração da Frequência de Luz!";
+console.log(descricao);
