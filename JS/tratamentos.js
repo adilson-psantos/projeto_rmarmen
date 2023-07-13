bab61a86d095dff73139baa9c25ec10f7ff25095
adilson-psantos <dipersan@gmail.com>
2023-07-12 23:15:58 -0300

    Atualizado CSS e darkmode de tratamentos

--- JS/tratamentos.js
+++ JS/tratamentos.js
@@ -5,7 +5,7 @@ const troca = document.querySelector(".texto-simples")
 troca.innerHTML = "Os Pleidianos são Seres de Luz estrelares que fazem parte da Constelação de Touro e atuam diretamente na consciência do Planeta Terra. Ligados também a Órion começaram a interferir para a manutenção do equilíbrio gravitacional e contribui para a elevação espiritual pela expansão da consciência que aos poucos se perde e desequilibra a Órbita do Eixo da Terra, afeta a humanidade. Como médicos energéticos do terceiro milênio, interferem naqueles com o DNA desperto para evolução que passam por vibração da Frequência de Luz!"
 console.log(troca);
 
-let titulo = document.getElementById(.titulo2)
+
 
 
 
