c1b285435ff1886afa948547869d850d2dbea0f6
Adilson Pereira dos Santos <dipersan@gmail.com>
2023-07-15 13:13:58 -0300

    adcionando descrição na pagina de tratamentos_desc que foi criada para detalhar os tratamentos

--- JS/tratamentos.js
+++
@@ -1,8 +0,0 @@
-let texto = document.getElementById("titulo");
-titulo.innerText = "ENERGIA FRIA VIBRACIONAL";
-console.log(titulo);
-
-let descricao = document.querySelector(".texto-simples");
-descricao.innerHTML =
-  "Os Pleidianos são Seres de Luz estrelares que fazem parte da Constelação de Touro e atuam diretamente na consciência do Planeta Terra. Ligados também a Órion começaram a interferir para a manutenção do equilíbrio gravitacional e contribui para a elevação espiritual pela expansão da consciência que aos poucos se perde e desequilibra a Órbita do Eixo da Terra, afeta a humanidade. Como médicos energéticos do terceiro milênio, interferem naqueles com o DNA desperto para evolução que passam por vibração da Frequência de Luz!";
-console.log(descricao);
---
+++ JS/tratamentos_desc.js
@@ -0,0 +1,38 @@
+const arrayTratamentos =[
+    {
+        titulo:"Energia Fria Vibracional",   
+        subtitulo : "Entendento melhor a Energia Fria Vibracional Curadora", 
+        texto : " Os Pleidianos são Seres de Luz estrelares que fazem parte da Constelação de Touro e atuam diretamente na consciência do Planeta Terra. Ligados também a Órion começaram a interferir para a manutenção do equilíbrio gravitacional e contribui para a elevação espiritual pela expansão da consciência que aos poucos se perde e desequilibra a Órbita do Eixo da Terra, afeta a humanidade. Como médicos energéticos do terceiro milênio, interferem naqueles com o DNA desperto para evolução que passam por vibração da Frequência de Luz"
+    },
+    {
+        titulo:"RADIESTESIA MAGNETICA IMANTADA",
+        subtitulo : "Entendendo melhor a Radiestesia Magnetica Imantada",
+        texto:"A princípio usamos o pêndulo de cristal como principal instrumento radiestésico, pois este emana uma frequência de ondas magnéticas e gravitacional de forma espiral em torno da pessoa, objeto ou local a ser trabalhado. No momento da aplicação a pessoa fica imantada, seu corpo começa se mover espontaneamente e a espiral de Luz entra em ação. Esta é a propriedade inicial da Radiestesia Magnética Imantada, os obstáculos inconscientes são removidos como que aspirados pelos teus chakras… assim é removido aos poucos o peso do medo e vem a conquista da abundância."
+    },
+    {
+        titulo:"REGISTRO DA MEMORIA CELULAR",
+        subtitulo: "Entendento Melhor o Registro da Memória Celular",
+        texto: "As células são responsáveis por toda nossa formação física, emocional e cerebral. Quando o espermatozoide atinge o óvulo na nossa concepção, o núcleo da célula se forma com todas as informações ancestrais (DNA). A partir desse momento as células se multiplicam e dá informação física… posteriormente o espírito acopla- se para a formação do embrião. A princípio predomina o físico; as informações instintivas vêm com o desenvolvimento do feto."
+    },
+]
+
+//criar um elemento
+let article = document.createElement("article")
+console.log(article)
+
+
+
+//Manipular Elemento 
+article.innerText = `
+<h3>titulo:"REGISTRO DA MEMORIA CELULAR"</h3>
+<p class = "subtitulo" ></p> "Entendento Melhor o Registro da Memória Celular"</p>
+<p> "As células são responsáveis por toda nossa formação física, emocional e cerebral. Quando o espermatozoide atinge o óvulo na nossa concepção, o núcleo da célula se forma com todas as informações ancestrais (DNA). A partir desse momento as células se multiplicam e dá informação física… posteriormente o espírito acopla- se para a formação do embrião. A princípio predomina o físico; as informações instintivas vêm com o desenvolvimento do feto."</p>`
+console.log(article)
+
+//Adcionando elemento na pagina
+
+
+
+
+
+
